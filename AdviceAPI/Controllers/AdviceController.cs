ae5b45f7a0c6076e4eb18ef1026fbead52217f88
Cheryl Mataitini <cheryl@Cheryls-MacBook-Pro-2.local>
2024-06-23 21:40:02 +0100

    initial commit

---
+++ AdviceAPI/Controllers/AdviceController.cs
@@ -0,0 +1,38 @@
+﻿using AdviceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace AdviceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdviceController : ControllerBase
+    {
+        IHttpClientFactory _httpClientFactory;
+
+        public AdviceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAdvice()
+        {
+            using HttpClient client = _httpClientFactory.CreateClient();
+
+            try
+            {
+                var res = await client.GetFromJsonAsync<AdviceReponse>(
+                    "https://api.adviceslip.com/advice",
+                    new JsonSerializerOptions(JsonSerializerDefaults.Web));
+
+                return Ok(res?.Slip?.Advice);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ AdviceAPI/Models/AdviceReponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace AdviceAPI.Models
+{
+    public class AdviceReponse
+    {
+        [JsonProperty("slip")]
+        public Slip Slip { get; set; }
+    }
+
+    public class Slip
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("advice")]
+        public string Advice { get; set; }
+    }
+}
